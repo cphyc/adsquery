c911da8b95c24cfd741977f985f63983805ea115
Corentin Cadiou <contact@cphyc.me>
2016-06-01 13:58:24 +0200

    update pypy version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='adsquery',
-      version='0.0.3-2',
+      version='0.0.4',
       description='Query the ADS using python',
       long_description=readme(),
       url='https://github.com/cphyc/adsquery',
