66f43a2d5d468f0da2c7626b71bbabe04564669a
Corentin Cadiou <contact@cphyc.me>
2016-06-04 20:49:52 +0200

    use pep8

--- adsquery/query.py
+++ adsquery/query.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals, print_function, division
 try:
     input = raw_input
-except NameError: # python3
+except NameError:  # python3
     pass
 
 # Local imports
@@ -54,15 +54,21 @@ def createQueryParser(parser):
     query_param = parser.add_argument_group('Query parameters')
 
     query_param.add_argument(metavar='query', dest='q', type=str, nargs='*',
-                             help='The search query. Can be either fielded (field:value) or unfielded (value).')
+                             help=('The search query. Can be either fielded '
+                                   '(field:value) or unfielded (value).'))
     query_param.add_argument('--rows', type=int, default=10,
                              help="number of results to return.")
     query_param.add_argument('--start', type=int, default=1,
-                             help="starting point for returned results (for pagination).")
+                             help=("starting point for returned results (for"
+                                   " pagination)."))
     query_param.add_argument('--fl', nargs='*',
-                             help="Fields list: specify the fields contained in each returned document. Value should be a comma-separated list of field names.")
+                             help=("Fields list: specify the fields contained"
+                                   " each returned document. Value should be a"
+                                   " comma-separated list of field names."))
     query_param.add_argument('--fq',
-                             help="Filter query: filter your results using a particular field:value condition. This parameter is repeatable.")
+                             help=("Filter query: filter your results using"
+                                   " in a particular field:value condition."
+                                   " This parameter is repeatable."))
     query_param.add_argument('--sort', type=str, nargs=2, help='Foo')
 
     fields = parser.add_argument_group('Fields')
@@ -75,17 +81,22 @@ def createQueryParser(parser):
     fields.add_argument('--alternate_bibcode',
                         help='list of alternate bibcodes for a single record')
     fields.add_argument('--alternate_title',
-                        help='list of alternate titles for a single record (usually if they are in multiple languages)')
+                        help=('list of alternate titles for a single record '
+                              '(usually if they are in multiple languages)'))
     fields.add_argument('--arxiv_class',
                         help='the arXiv class the pre-print was submitted to')
     fields.add_argument('--author',
-                        help='an array of the author names associated with the record')
+                        help=('an array of the author names associated with'
+                              ' the record'))
     fields.add_argument('--bibcode',
-                        help='the canonical ADS bibcode identifier for this record')
+                        help=('the canonical ADS bibcode identifier for this'
+                              ' record'))
     fields.add_argument('--bibgroup',
-                        help='the bibliographic groups that the bibcode has been associated with')
+                        help=('the bibliographic groups that the bibcode has'
+                              ' been associated with'))
     fields.add_argument('--bibstem',
-                        help='the abbreviated name of the journal or publication, e.g., ApJ.')
+                        help=('the abbreviated name of the journal or,'
+                              ' publication e.g., ApJ.'))
     fields.add_argument('--body',
                         help='the full text content of the article')
     fields.add_argument('--citation_count',
@@ -93,27 +104,34 @@ def createQueryParser(parser):
     fields.add_argument('--copyright',
                         help='the copyright applied to the article')
     fields.add_argument('--data',
-                        help='the list of sources that have data related to this bibcode')
+                        help=('the list of sources that have data related to'
+                              ' this bibcode'))
     fields.add_argument('--database',
                         help='Which database the record is associated with.')
     fields.add_argument('--doi',
                         help='the digital object identifier of the article')
     fields.add_argument('--doctype',
-                        help='the type of document it is (see here for a list of doctypes)')
+                        help=('the type of document it is (see here for a list'
+                              ' of doctypes)'))
     fields.add_argument('--first-author',
                         help='the first author of the article')
     fields.add_argument('--grant',
-                        help='the list of grant IDs and agencies noted within an article')
+                        help=('the list of grant IDs and agencies noted within'
+                              ' an article'))
     fields.add_argument('--id',
-                        help='a (non-persistent) unique integer for this record, used for fast look-up of a document')
+                        help=('a (non-persistent) unique integer for this'
+                              ' record, used for fast look-up of a document'))
     fields.add_argument('--identifier',
-                        help='an array of alternative identifiers for the record. May contain alternative bibcodes, DOIs and/or arxiv ids.')
+                        help=('an array of alternative identifiers for the'
+                              ' record. May contain alternative bibcodes, DOIs'
+                              ' and/or arxiv ids.'))
     fields.add_argument('--indexstamp',
                         help='time at which the document was (last) indexed')
     fields.add_argument('--issue',
                         help='issue the record appeared in')
     fields.add_argument('--keyword',
-                        help='an array of normalized and un-normalized keyword values associated with the record')
+                        help=('an array of normalized and un-normalized'
+                              ' keyword values associated with the record'))
     fields.add_argument('--lang*',
                         help='the language of the article s title')
     fields.add_argument('--orcid-pub',
@@ -121,17 +139,23 @@ def createQueryParser(parser):
     fields.add_argument('--orcid-user',
                         help='ORCiD iDs supplied by knonwn users in the ADS')
     fields.add_argument('--orcid-other',
-                        help='ORCiD iDs supplied by anonymous users in the ADS')
+                        help=('ORCiD iDs supplied by anonymous users in the'
+                              ' ADS'))
     fields.add_argument('--page',
                         help='starting page')
     fields.add_argument('--property',
-                        help='an array of miscellaneous flags associated with the record (see here for a list of properties')
+                        help=('an array of miscellaneous flags associated with'
+                              ' the record (see here for a list of '
+                              'properties'))
     fields.add_argument('--pub',
-                        help='the canonical name of the publication the record appeared in')
+                        help=('the canonical name of the publication the'
+                              ' record appeared in'))
     fields.add_argument('--pubdate',
-                        help='publication date in the form YYYY-MM-DD (DD value will always be "00")')
+                        help=('publication date in the form YYYY-MM-DD (DD '
+                              'value will always be "00")'))
     fields.add_argument('--read-count',
-                        help='number of times the record has been viewed within in a 90-day windows (ads and arxiv)')
+                        help=('number of times the record has been viewed '
+                              'within in a 90-day windows (ads and arxiv)'))
     fields.add_argument('--title',
                         help='the title of the record')
     fields.add_argument('--vizier',
@@ -147,7 +171,8 @@ def createGetParser(parser):
 
     """
     parser.add_argument('--bibcode',
-                        help='the canonical ADS bibcode identifier for this record')
+                        help=('the canonical ADS bibcode identifier for '
+                              'this record'))
     return
 
 
@@ -163,7 +188,8 @@ def printResults(results):
 
         # Format author
         if res.author is not None and len(res.author) > 3:
-            authors = res.author[0] + ' et al. ({} more)'.format(len(res.author[1:]))
+            authors = res.author[0] + \
+                ' et al. ({} more)'.format(len(res.author[1:]))
         elif res.author is None:
             authors = 'No author'
         else:
@@ -178,8 +204,10 @@ def printResults(results):
             pub = '?'
 
         # Format year journal
-        print('{c.HEADER}[{i}]: {c.OKGREEN}{year}, {pub} — {c.OKBLUE}{author}{c.ENDC}, {title}'.format(
-            i=i, year=res.year, title=res.title[0], author=authors, c=bcolors, pub=pub))
+        print('{c.HEADER}[{i}]: {c.OKGREEN}{year}, {pub} — {c.OKBLUE}'
+              '{author}{c.ENDC}, {title}'.format(
+                  i=i, year=res.year, title=res.title[0], author=authors,
+                  c=bcolors, pub=pub))
 
 
 def doQuery(args, **kwargs):
@@ -198,7 +226,7 @@ def doQuery(args, **kwargs):
     res_as_array = []
 
     # loop over results
-    try: # sometimes, the ads script gets an IndexError, hide this
+    try:  # sometimes, the ads script gets an IndexError, hide this
         res_as_array = [res for res in results]
     except IndexError:
         res_ar_array = []
@@ -208,10 +236,12 @@ def doQuery(args, **kwargs):
         print('No results')
         return
 
-
     if args.interactive or ('interactive' in kwargs and kwargs['interactive']):
         print('')
-        inp = input('Comma separated articles to download [e.g. 1-3, 4], [m] for more [q] to quit or add more parameters to request [e.g. year:2016]: ')
+        inp = input(
+            'Comma separated articles to download [e.g. 1-3, 4], [m] for more'
+            ' [q] to quit or'
+            ' add more parameters to request [e.g. year:2016]: ')
 
         # Match quit request
         if inp == 'q':
@@ -233,7 +263,6 @@ def doQuery(args, **kwargs):
             doQuery(args, **kwargs)
             return
 
-
         # Match selection
         mask = parseAsList(inp)
         if len(mask) > 0:
@@ -243,7 +272,6 @@ def doQuery(args, **kwargs):
 
             action = getInput(
                 'Download [d], bibtex[b], quit[q]? ', lambda e: e.lower())
-
 
             # Download selected articles
             if 'd' in action:
@@ -258,7 +286,7 @@ def doQuery(args, **kwargs):
                 print(''.join(bibtex))
 
             return papers
-        else: # match more request
+        else:  # match more request
             if args.q is not None:
                 args.q = args.q + ' ' + inp
             elif 'q' in kwargs:
@@ -275,8 +303,10 @@ def downloadPaper(paper):
     '''Download the paper.
     :arg paper
     a result given by the ads'''
-    url = "http://adsabs.harvard.edu/cgi-bin/nph-data_query?bibcode={paper.bibcode}&link_type=ARTICLE".format(
-        paper=paper)
+    url = ("http://adsabs.harvard.edu/cgi-bin/nph-data_query?"
+           "bibcode={paper.bibcode}&link_type=ARTICLE".format(
+               paper=paper))
+
     fname = '{paper.bibcode}_{author}.pdf'.format(
         paper=paper,
         author=paper.first_author.split(',')[0])
@@ -290,7 +320,8 @@ def downloadPaper(paper):
                 total_length = int(r.headers.get('content-length'))
             except:
                 total_length = -1
-            for chunk in tqdm(r.iter_content(chunk_size=1024), total=(total_length / 1024)):
+            for chunk in tqdm(r.iter_content(chunk_size=1024),
+                              total=(total_length / 1024)):
                 if chunk:
                     f.write(chunk)
                     f.flush()
@@ -353,9 +384,10 @@ def main():
     parser.add_argument('--no-interactive', help='Deactivate interaction',
                         dest='interactive', action='store_false')
     parser.add_argument('--token', type=str,
-                        help='Your ADS token. Leave empty to use the value from ~/.ads/dev_key or from the environment variable ADS_DEV_KEY.',
+                        help=('Your ADS token. Leave empty to use the value '
+                              'from ~/.ads/dev_key or from the environment '
+                              'variable ADS_DEV_KEY.'),
                         default=None)
-
 
     subparsers = parser.add_subparsers()
     query_parser = subparsers.add_parser('query', description='Query the ADS')
@@ -376,10 +408,9 @@ def main():
     if 'q' in vars(args):
         args.q = ' '.join(args.q)
 
-
     try:
         return args.func(args)
-    except AttributeError: # func not in namespace
+    except AttributeError:  # func not in namespace
         parser.print_help()
         return
 
